259507094ec0f1c702b992fb9d73ae44c2b46e3e
tenedict <tenedict@gmail.com>
2022-09-30 10:31:48 +0900

    0930프로젝트

--- movie_pjt/movies/views.py
+++ movie_pjt/movies/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Movie
 
 # Create your views here.
+
+
+def index(request):
+    movies = Movie.objects.all()
+
+    context = {
+        "movies": movies,
+    }
+    return render(request, "movies/index.html", context)
+
+
+def neww(request):
+    content = request.GET.get("content")
+    title = request.GET.get("title")
+
+    Movie.objects.create(
+        content=content,
+        title=title,
+    )
+
+    return redirect("movies:index")
